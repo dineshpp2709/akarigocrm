d6763da952c656edde01e58fa8cb2809259a2227
dineshpp2709 <45043851+dineshpp2709@users.noreply.github.com>
2021-08-27 16:02:32 +0530

    V2 Commit

---
+++ BritishLibrary-master/library_app/__manifest__.py
@@ -0,0 +1,15 @@
+{'name': 'Library Management Application',
+ 'description': 'Library books, members and book borrowing.',
+ 'author': 'Dinesh Patankar',
+ 'depends': ['base'],
+ 'data': [
+    'security/library_security.xml',
+    'security/ir.model.access.csv',
+    'views/library_menu.xml',
+    'views/book_view.xml',
+    'views/book_list_template.xml',
+    #'data/ir_ui_view.xml',
+ ],
+ 'application': True,
+ 'installable': True,
+ }
